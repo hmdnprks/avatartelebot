863fe5748ce68ebdf79852eccb104aa216686ae0
Hamdan Prakoso <hamdan.prakoso007@gmail.com>
2021-01-26 09:56:05 +0700

    Change base dir var

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import telegram
 import os
 import dotenv
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
   dotenv.load_dotenv(dotenv_file)
